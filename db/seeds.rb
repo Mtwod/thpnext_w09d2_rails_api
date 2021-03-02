db4da72e30646a8f0d9908372387e3211014adf0
Mtwod <maximewong@hotmail.com>
2021-03-02 21:00:25 +0100

    FastForward of tutorial steps till end with relation User Articles

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+5.times do |i|
+  User.create(email: "prenomnom#{i}@tst.tst", password: '123456')
+end
+
+20.times do
+  Article.create(
+    title: Faker::Movies::BackToTheFuture.character,
+    content: Faker::Movies::BackToTheFuture.quote,
+    user: User.all.sample,
+  )
+end
