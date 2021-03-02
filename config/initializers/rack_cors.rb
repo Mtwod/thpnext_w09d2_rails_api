95dc406529f0bfc2f2f53039393cacf611d80c9c
Mtwod <maximewong@hotmail.com>
2021-03-02 20:53:04 +0100

    FastForward of tutorial steps before test

---
+++ config/initializers/rack_cors.rb
@@ -0,0 +1,9 @@
+# In prod, best to limit origin to an Allowlist
+Rails.application.config.middleware.insert_before 0, Rack::Cors do
+  allow do
+    origins '*'
+    resource '*',
+		headers: :any,
+		methods: [:get, :post, :patch, :put, :options]
+  end
+end
